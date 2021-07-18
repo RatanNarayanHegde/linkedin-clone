6fbe42afe7e17166745788509fee9b166de7d774
Ratan Narayan Hegde <ratanhegde08@gmail.com>
2021-07-18 11:06:37 +0530

    add header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
   return (
@@ -17,7 +23,17 @@ function Header() {
         </div>
       </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://pbs.twimg.com/profile_images/1400072430651990019/CXiDyfBf_400x400.jpg"
+          title="me"
+        />
+      </div>
     </div>
   );
 }
