a01df010ea94e10074a99e92009eb3c2e4cafaac
Ratan Narayan Hegde <ratanhegde08@gmail.com>
2021-07-18 18:19:00 +0530

    style post

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
+import InputOption from "./InputOption";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ChatOutlinedIcon from "@material-ui/icons/ChatOutlined";
+import ShareOutlinedIcon from "@material-ui/icons/ShareOutlined";
+import SendOutlinedIcon from "@material-ui/icons/SendOutlined";
 
 function Post({ name, description, message, photoUrl }) {
   return (
@@ -9,11 +14,17 @@ function Post({ name, description, message, photoUrl }) {
         <Avatar src={photoUrl} />
         <div className="post__info">
           <h2>Ratan Hegde</h2>
-          <p>Description</p>
+          <p>{description}</p>
         </div>
       </div>
       <div className="post__body">
-        <p>Message goes here{message}</p>
+        <p>{message}</p>
+      </div>
+      <div className="post__buttons">
+        <InputOption Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray" />
+        <InputOption Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+        <InputOption Icon={ShareOutlinedIcon} title="Share" color="gray" />
+        <InputOption Icon={SendOutlinedIcon} title="Send" color="gray" />
       </div>
     </div>
   );
