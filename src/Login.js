f86eabef1507a9214abc9d655dde3f0ff71ada36
Ratan Narayan Hegde <ratanhegde08@gmail.com>
2021-07-19 19:00:08 +0530

    add login form

---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <img
+        src="https://logos-download.com/wp-content/uploads/2016/03/LinkedIn_Logo_2003.png"
+        alt=""
+      />
+      <form action=""></form>
+    </div>
+  );
+}
+
+export default Login;
